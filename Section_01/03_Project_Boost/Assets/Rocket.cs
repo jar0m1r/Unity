04d826f861b226631c8b8684f6c5a3d0138cbd5a
Jaromir Humbitious <jaromir.vangorsel@gmail.com>
2018-04-10 09:39:20 +0200

    Add AudioSource to Rocket

--- Section_01/03_Project_Boost/Assets/Rocket.cs
+++ Section_01/03_Project_Boost/Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,8 +21,16 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space))
+        {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A)){
